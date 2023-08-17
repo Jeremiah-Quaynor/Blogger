2dc3b3c549f5b24c785d4e04b071c7aa76101e64
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2023-08-17 20:28:26 +0000

    adding layout

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h4>Hello, World!</h4>'
+    return render_template('home.html', posts= posts,)
 
 
 @app.route('/about')
 def about():
-    return '<h4>about page, World!</h4>'
+    return render_template("about.html", title='About')
 
 
 if __name__ == '__main__':
