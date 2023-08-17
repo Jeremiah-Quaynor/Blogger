ce898f71c769e91d1a95cdc50225e0bcf9a30461
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2023-08-17 22:51:10 +0000

    adding forms and for validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'f4ee190cd02a0c1466d73bc03852cf5f'
 
 posts = [
     {
@@ -21,12 +22,37 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts= posts,)
+    return render_template('home.html', posts=posts,)
 
 
 @app.route('/about')
 def about():
     return render_template("about.html", title='About')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'account created for {form.username}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        elif form.email.data != "admin@blog.com" and form.password.data == "password":
+            flash('Login unsuccessful. Please check username', 'danger')
+        elif form.password.data != "password" and form.email.data == "admin@blog.com":
+            flash('Login unsuccessful. Please check password', 'danger')
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
